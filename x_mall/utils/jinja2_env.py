d62b7d593ce70b3dbadde9141b5a11bb923318c5
xxwl <xxwl@duee.cn>
2019-04-16 17:08:01 +0800

    '初始项目'

--- x_mall/utils/jinja2_env.py
+++ x_mall/utils/jinja2_env.py
@@ -8,7 +8,18 @@
 @time: 2019/4/16 16:04
 """
 
-
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import reverse
 from jinja2 import Environment
+
+
+def jinja2_environment(**options):
+    """
+    可以使用Django模板引擎中的{% url('') %} {% static('') %}这类的语句
+    """
+    env = Environment(**options)
+    env.globals.update({
+        'static': staticfiles_storage.url,
+        'url': reverse,
+    })
+    return env
